f574bccdb073eb358d421beda0375a9add303863
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-01-03 14:45:36 +0500

    changed api new for new shopify app.

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "signup test"
-  config.api_key = "00b57aae9dc9b806b2a97c107abddb78"
-  config.secret = "abb0cfcb0ac48f654df36cd8bd682b94"
+  config.api_key = "331cb584654d6d2c9a6334de943c17c9"
+  config.secret = "11a06c64579d575516b8e0519ce8688f"
   config.scope = "read_orders, read_products, write_script_tags"
   config.embedded_app = true
   config.scripttags = [
