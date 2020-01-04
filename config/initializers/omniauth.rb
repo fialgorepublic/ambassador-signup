b30025c77c809ec63d80143cbde8a8bf30c872d6
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-01-04 22:35:34 +0500

    minor fix.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
     scope: ShopifyApp.configuration.scope,
-    redirect_uri: 'https://4d554604.ngrok.io/auth/shopify/callback​'
+    redirect_uri: 'https://popup.saintlbeau.com/auth/shopify/callback​'
 end
