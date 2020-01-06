7bca21b29fe13f6357de399afddaf8b57d002da0
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-01-06 19:28:16 +0500

    minor fix.

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,8 +2,8 @@ ShopifyApp.configure do |config|
   config.application_name = "POP UP Saint L Beau"
   # config.api_key = "adf65d5dca67061cffad7d8c4a9f9532"
   # config.secret = "743c55dc05deda73900466e0b2d47684"
-  config.api_key = "00b57aae9dc9b806b2a97c107abddb78"
-  config.secret = "abb0cfcb0ac48f654df36cd8bd682b94"
+  config.api_key = "adf65d5dca67061cffad7d8c4a9f9532"
+  config.secret = "743c55dc05deda73900466e0b2d47684"
   config.scope = "read_orders, read_products, write_script_tags"
   config.embedded_app = true
   config.scripttags = [
