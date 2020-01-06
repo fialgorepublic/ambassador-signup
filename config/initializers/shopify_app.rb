9d6a2c61246cf9504b70f7a1824058de48e1baab
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-01-06 12:33:05 +0500

    change app name.

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "signup test"
+  config.application_name = "POP UP Saint L Beau"
   # config.api_key = "adf65d5dca67061cffad7d8c4a9f9532"
   # config.secret = "743c55dc05deda73900466e0b2d47684"
   config.api_key = "331cb584654d6d2c9a6334de943c17c9"
