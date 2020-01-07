5b50ff55ca301bcf2dc694ffbf1b07e909943f7b
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-01-07 16:48:13 +0500

    minor fix.

--- public/custom.js
+++ public/custom.js
@@ -1,25 +1,25 @@
-// $('head').append('<link rel="stylesheet" href="https://975ffa93.ngrok.io/file.css">' +
-//     '<link rel=\'stylesheet prefetch\' href=\'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\'>')
-// $('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>' + '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>')
-// var html = '<button type="button" class="btn btn-info btn-lg" data-toggle="modal" id="myid" data-target="#myModal" style="display: none;">Click here </button>' + '<div class="modal fade" id="myModal" role="dialog" style="background-color: transparent;">'+'<div class="modal-dialog">'+
-//     '<div class="modal-content">'+
-//       '<div class="modal-header">'+
-//         '<button type="button" class="close" data-dismiss="modal">&times;</button>'+
-//       '</div>'+
-//       '<div class="modal-body text-center">'+
-//         '<h4 class=""><b>Signup Now</b> to get free Shipping</h4>'+
-//       '</div>'+
-//       '<div class="modal-footer">'+
-//         '<a href="https://ambassador.saintlbeau.com/?signup=true" class="btn btn-success" role="button", >Signup</a>'+
-//         '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>'+
-//       '</div>'+
-//     '</div>'+
-//   '</div>'+
-// '</div>'
-// $('body').append(html)
-// $(document).ready(function(){
-//   button = document.getElementById('myid')
-//   setTimeout(function() {
-//   $(button).click();
-// },10000);
-// })
+$('head').append('<link rel="stylesheet" href="https://975ffa93.ngrok.io/file.css">' +
+    '<link rel=\'stylesheet prefetch\' href=\'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\'>')
+$('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>' + '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>')
+var html = '<button type="button" class="btn btn-info btn-lg" data-toggle="modal" id="myid" data-target="#myModal" style="display: none;">Click here </button>' + '<div class="modal fade" id="myModal" role="dialog" style="background-color: transparent;">'+'<div class="modal-dialog">'+
+    '<div class="modal-content">'+
+      '<div class="modal-header">'+
+        '<button type="button" class="close" data-dismiss="modal">&times;</button>'+
+      '</div>'+
+      '<div class="modal-body text-center">'+
+        '<h4 class=""><b>Signup Now</b> to get free Shipping</h4>'+
+      '</div>'+
+      '<div class="modal-footer">'+
+        '<a href="https://ambassador.saintlbeau.com/?signup=true" class="btn btn-success" role="button", >Signup</a>'+
+        '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>'
+$('body').append(html)
+$(document).ready(function(){
+  button = document.getElementById('myid')
+  setTimeout(function() {
+  $(button).click();
+},10000);
+})
