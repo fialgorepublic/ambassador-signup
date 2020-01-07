438c7684224e83e4a7fc8fe058f7e93f65adebf6
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-01-07 16:17:20 +0500

    changed api key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,8 +2,8 @@ ShopifyApp.configure do |config|
   config.application_name = "POP UP Saint L Beau"
   # config.api_key = "adf65d5dca67061cffad7d8c4a9f9532"
   # config.secret = "743c55dc05deda73900466e0b2d47684"
-  config.api_key = "a48c107f75f2d30077c8b62e43b1cf17"
-  config.secret = "1f4552d81abcbd5cd8d4236d1e7eb09a"
+  config.api_key = "cc28616746f59726098303b177024d3b"
+  config.secret = "267985dd75678467d39a69a8cdb89746"
   config.scope = "read_orders, read_products, write_script_tags"
   config.embedded_app = true
   config.scripttags = [
